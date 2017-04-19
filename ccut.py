69f686b05848ed136751e494a016012056a3ffd5
Ruud van Asseldonk <dev@veniogames.com>
2017-04-19 23:15:41 +0200

    Add Python implementation for comparison
    It is about twice as slow, and consumes half a GiB of memory at peak,
    whereas the Rust version only consumes 6 MiB.

---
+++ ccut.py
@@ -0,0 +1,22 @@
+#!/bin/env python3
+
+"""
+A very simple Python implementation that is supposed to yield the same output as
+the Rust program. But beware of the differences:
+
+ * What happens when the wrong number of arguments is provided on the command
+   line?
+ * What happens when the column is not an integer? When it is negative?
+ * What happens when a line contains less columns than requested?
+ * How much memory does it consume? What if the input file contains a long line?
+
+"""
+
+import sys
+
+column = int(sys.argv[1])
+ifname = sys.argv[2]
+
+with open(ifname, 'r') as f:
+    for line in f:
+        print(line.split(',')[column])
--- gencsv.py
+++ gencsv.py
@@ -2,10 +2,11 @@
 
 with open('/tmp/large.csv', 'w') as f:
     f.write('integer,divisible_by_3,divisible_by_7,comment\n')
-    for i in range(0, 1024 * 1024):
+    for i in range(0, 16 * 128):
         div3 = 'yes' if i % 3 == 0 else 'no'
         div7 = 'yes' if i % 7 == 0 else 'no'
-        # Occasionally make a line with a very long comment. This makes
-        # line-by-line streaming infeasible, because lines can be very long.
-        comment = ('very long comment' * 8192) if i % 239 == 39 else ''
+        # Occasionally make a line with a very long comment (~128 MiB). This
+        # makes line-by-line streaming infeasible, because lines can be very
+        # long.
+        comment = ('x' * 1024 * 1024 * 128) if i % 128 == 64 else ''
         f.write('{},{},{},{}\n'.format(i, div3, div7, comment))
