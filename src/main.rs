45aec4193b888bc3930dee27442122a50869e32b
Ruud van Asseldonk <dev@veniogames.com>
2017-04-19 22:49:45 +0200

    Csv to json has too many edge cases, do cut instead

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Csvtojson -- Basic example for the Rust Utrecht meetup.
+// Ccut -- Basic example for the Rust Utrecht meetup.
 // Written in 2017 by Ruud van Asseldonk
 //
 // To the extent possible under law, the author(s) have dedicated all copyright
@@ -7,128 +7,106 @@
 // received a copy of the CC0 Public Domain Dedication along with this software.
 // If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+// This file implements a cut-like program that outputs a given (0-based) column
+// of a file of comma-separated values. Exercises left to the reader:
+//
+//  * Allow writing to a file in addition to stdout.
+//  * Allow reading from stdin in addition to a file.
+//  * Allow the user to specify the separator.
+//  * Allow the user to select a contiguous range of columns.
+//  * Allow the user to select an arbitrary subset of columns.
+//  * Speed up the program using the memchr crate.
+//  * Allow using non-ASCII separators, assuming UTF-8 input.
+//    The application should deal with code points spanning the buffer boundary.
+//  * Allow using UTF-16 input.
+
 use std::env;
 use std::fs::File;
 use std::io::{BufWriter, Read, Write};
 use std::io;
+use std::str::FromStr;
 
 fn main() {
     let mut args = env::args();
     if args.len() != 3 {
-        println!("Usage:\n\n  csvtojson <infile> <outfile>");
+        println!("Usage:\n\n  ccut <column> <infile>");
         return
     }
 
-    // These unwraps are safe, because three arguments are given.
+    // These unwraps are safe, because three arguments are given. (The first one
+    // is the name of the program.)
     args.next();
+    let colstr = args.next().unwrap();
     let ifname = args.next().unwrap();
-    let ofname = args.next().unwrap();
+
+    let column = u32::from_str(&colstr)
+        .expect("Column must be a non-negative integer.");
 
     let ifile = File::open(ifname).expect("Failed to open input file.");
-    let ofile = File::create(ofname).expect("Failed to open output file.");
-    process_file(ifile, ofile).expect("Failed to process file.");
+    process_file(column, ifile).expect("Failed to process file.");
 }
 
-fn process_file(mut input: File, raw_output: File) -> io::Result<()> {
-    // Wrap the file in a `BufWriter`, so we don't need to do a syscall for
-    // every write operation, but only when the 4 KiB buffer is full.
-    let mut output = BufWriter::with_capacity(4096, raw_output);
+fn process_file(column: u32, mut input: File) -> io::Result<()> {
+    // Take a lock on stdout, such that write calls don't need to lock while the
+    // `handle` is in scope. This program is not multithreaded anyway.
+    let stdout = io::stdout();
+    let handle = stdout.lock();
 
-    // Fill a 4 KiB input buffer once, and locate the first newline.
+    // Wrap the sink in a `BufWriter`, so we don't need to do a syscall for
+    // every write operation, but only when the 4 KiB buffer is full. The 4 KiB
+    // buffer is allocated on the heap.
+    let mut output = BufWriter::with_capacity(4096, handle);
+
+    // Allocate a 4 KiB input buffer on the stack.
     let mut buffer = [0u8; 4096];
-    let mut len = input.read(&mut buffer)?;
-    let first_newline = find_byte(&buffer[..len], b'\n')
-        .expect("Header row must fit in 4 KiB.");
 
-    // Store the column headers, because we need to output them every time.
-    let column_headers = parse_header(&buffer[..first_newline]);
+    let mut current_column = 0;
+    let mut should_echo = column == current_column;
 
-    // Write the start of the json array.
-    write!(output, "[")?;
-
-    // The index of the current header.
-    let mut h = 0;
-    let mut needs_row_sep = true;
-    let mut needs_header = true;
-    let mut row_sep = "";
+    // Keep track of the first byte of the desired column.
+    let mut from = 0;
 
     loop {
-        for byte in &buffer[first_newline + 1..len] {
-            match *byte {
-                b'\n' => {
-                    // Note: the double } escapes the format string.
-                    write!(output, "\"}}")?;
-                    // We should only write row separators after the first row;
-                    // initially it was empty.
-                    row_sep = ",";
-                    needs_row_sep = true;
-                    needs_header = true;
-                    h = 0;
-                }
-                b',' => {
-                    write!(output, "\",")?;
-                    h += 1;
-                    if h >= column_headers.len() {
-                        panic!("Data row contains more columns than header row.");
-                    }
-                    // We should only write row separators after the first row;
-                    // initially it was empty.
-                    row_sep = ",";
-                    needs_header = true;
-                }
-                c => {
-                    if needs_row_sep {
-                        // Note: the double { escapes the format string.
-                        write!(output, "{}{{", row_sep)?;
-                        needs_row_sep = false;
-                    }
-                    if needs_header {
-                        write!(output, "\"{}\":\"", column_headers[h])?;
-                        needs_header = false;
-                    }
-                    // Do not use the `write!` macro to write the byte, as that
-                    // would write it as digits. We could do proper decoding and
-                    // work with strings, at the cost of validating UTF-8.
-                    // Instead, just output the byte. Write a slice of length 1.
-                    output.write(&[c])?;
-                }
+        let len = input.read(&mut buffer)?;
 
-            }
-        }
-
-        // Refill the buffer with new data.
-        len = input.read(&mut buffer)?;
-
-        // When the number of bytes read is 0, we reached EOF.
+        // When `read()` returns 0, we have reached EOF.
         if len == 0 {
             break
         }
-    }
 
-    // If the csv file did not end in a newline, we must still close the last
-    // field and the object.
-    if !needs_row_sep {
-        write!(output, "\"}}")?;
-    }
+        for i in 0..len {
+            match buffer[i] {
+                b',' => {
+                    if should_echo {
+                        output.write(&buffer[from..i])?;
+                    }
 
-    // CLose the json array.
-    write!(output, "]")?;
+                    current_column += 1;
+                    should_echo = column == current_column;
+                    from = i + 1;
+                }
+                b'\n' => {
+                    if should_echo {
+                        output.write(&buffer[from..i])?;
+                    }
+
+                    current_column = 0;
+                    should_echo = column == current_column;
+                    from = i + 1;
+
+                    write!(output, "\n")?;
+                }
+                _ => {}
+            }
+        }
+
+        // We might have reached the end of the buffer inside a column that
+        // should be printed.
+        if should_echo {
+            output.write(&buffer[from..len])?;
+        }
+        from = 0;
+    }
 
     Ok(())
-}
-
-fn find_byte(buffer: &[u8], byte: u8) -> Option<usize> {
-    for i in 0..buffer.len() {
-        if buffer[i] == byte {
-            return Some(i)
-        }
-    }
-    None
-}
-
-fn parse_header(header_row: &[u8]) -> Vec<String> {
-    header_row
-        .split(|byte_ptr| *byte_ptr == b',')
-        .map(|header| String::from_utf8_lossy(header).into_owned())
-        .collect()
 }
