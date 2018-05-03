0599b5b0147c95439122f6744d2392fa54a7ab87
Ruud van Asseldonk <dev@veniogames.com>
2018-05-03 20:57:33 +0200

    Simplify line iteration a bit

--- 2018-05/grep/src/main.rs
+++ 2018-05/grep/src/main.rs
@@ -2,20 +2,16 @@ extern crate regex;
 
 use std::env;
 use std::io;
+use std::io::BufRead;
 
 fn main() {
     let pattern = env::args().skip(1).next().expect("No pattern provided");
     let re = regex::Regex::new(&pattern).expect("Invalid regex");
     let stdin = io::stdin();
-    let mut line = String::new();
-    loop {
-        line.clear();
-        if let Ok(n) = stdin.read_line(&mut line) {
-            if re.is_match(&line) {
-                print!("{}", line);
-            }
-            if n == 0 {
-                break
+    for line in stdin.lock().lines() {
+        if let Ok(ln) = line {
+            if re.is_match(&ln) {
+                println!("{}", ln);
             }
         }
     }
