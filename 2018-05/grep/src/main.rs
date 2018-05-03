cbdad9d23d3cf3fafacdedf9fcdc7b8903baf6b2
Ruud van Asseldonk <dev@veniogames.com>
2018-05-03 21:16:33 +0200

    Highlight matches

--- 2018-05/grep/src/main.rs
+++ 2018-05/grep/src/main.rs
@@ -8,10 +8,24 @@ fn main() {
     let pattern = env::args().skip(1).next().expect("No pattern provided");
     let re = regex::Regex::new(&pattern).expect("Invalid regex");
     let stdin = io::stdin();
+    // ANSI escape codes for bold red, and reset.
+    let make_red = "\x1b[31;1;1m";
+    let reset = "\x1b[0m";
     for line in stdin.lock().lines() {
         if let Ok(ln) = line {
-            if re.is_match(&ln) {
-                println!("{}", ln);
+            let mut k = 0;
+            let mut highlighted = ln.clone();
+            // Iterate over all matches, and insert turn-red code before the
+            // start of a match, insert a reset code after the match.
+            for m in re.find_iter(&ln) {
+                highlighted.insert_str(m.start() + k, make_red);
+                k += make_red.len();
+                highlighted.insert_str(m.end() + k, reset);
+                k += reset.len();
+            }
+            if k > 0 {
+                // Print the line if there was a match.
+                println!("{}", highlighted);
             }
         }
     }
