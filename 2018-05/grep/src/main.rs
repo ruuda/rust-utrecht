0958d77270ff94030ef0c0d99841bba64600388c
Ruud van Asseldonk <dev@veniogames.com>
2018-05-03 20:28:19 +0200

    Make program print stdin

--- 2018-05/grep/src/main.rs
+++ 2018-05/grep/src/main.rs
@@ -1,3 +1,19 @@
+extern crate regex;
+
+use std::env;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = env::args().skip(1).next().unwrap();
+    let mut stdin = io::stdin();
+    let mut line = String::new();
+    loop {
+        if let Ok(n) = stdin.read_line(&mut line) {
+            print!("{}", line);
+            if n == 0 {
+                break
+            }
+            line.clear();
+        }
+    }
 }
